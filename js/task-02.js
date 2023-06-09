4d89249ca38354145ce6821eceb1c97ed0d7780a
charivna <124678194+charivna@users.noreply.github.com>
2023-06-09 14:52:42 +0200

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,31 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+
+const vegetableEl = document.querySelector('#ingredients')
+
+const elements = ingredients.map(ingredient => {
+  
+  const ingredientEl = document.createElement('li');
+  ingredientEl.classList.add('item');
+  ingredientEl.textContent = ingredient;
+
+  return ingredientEl
+}
+)
+vegetableEl.append(...elements)
+console.log(elements);
+
+// for (let i = 0; i < ingredients.length; i += 1){
+  
+//   const ingredientEl = document.createElement('li');
+//   ingredientEl.classList.add('item');
+//   ingredientEl.textContent = ingredients[i];
+//   console.log(ingredients[i]);
+//   elements.push(ingredientEl)
+// }
+// vegetableEl.append(...elements)
+// console.log(vegetableEl);
+
+
