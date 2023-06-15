53334051cfbb4f6e56f717331ccf59f2160f615a
charivna <124678194+charivna@users.noreply.github.com>
2023-06-15 16:56:36 +0200

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,28 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+
+const btnCreateEl = document.querySelector('button[data-create]')
+console.log(btnCreateEl);
+const btnDestroyEl = document.querySelector('button[data-destroy]')
+console.log(btnDestroyEl);
+const inputEl = document.querySelector('input')
+console.log(inputEl.value);
+const boxesEl = document.querySelector('#boxes')
+
+btnCreateEl.addEventListener('click', createBoxes);
+btnDestroyEl.addEventListener('click',destroyBoxes)
+
+function createBoxes(event) {
+  const itemsEl = document.createElement('div')
+
+  console.log(itemsEl);
+  
+  boxesEl.append(itemsEl)
+}
+
+
+function destroyBoxes(event) {
+  
+}
